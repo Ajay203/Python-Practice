b944fbdf78f0351e221d0f6a0eed3b8b720eabe5
Ajay Sharma <>
2022-11-28 21:05:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#Write your code below this line 👇
+print ("Day 1 - Python print Function")
+print ("The fuction is declared like this:")
+print ("print('what to print')")
+
+print ("Hellow world !\nI am new here.")
+
+print ("Hello " + "Ajay")
+
+
+
+
+
+
+
